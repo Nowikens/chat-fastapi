0c3f6789166158ea882e18eba09b4e93ff5750fb
Nowikens <kuban96166@gmail.com>
2024-06-24 20:30:00 +0200

    fix: return user after succesfull authentication

--- user/utils/auth.py
+++ user/utils/auth.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, HTTPException
+from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from sql import get_db
@@ -7,7 +7,7 @@ from user.schemas import UserLogin
 from user.utils.password import verify_password
 
 
-def authenticate_user(user: UserLogin, db: Session = Depends(get_db)) -> bool:
+def authenticate_user(user: UserLogin, db: Session = Depends(get_db)) -> User | bool:
     """Checks if user with given email exists and if provided password is valid
     """
     db_user = db.query(User).filter(
@@ -17,4 +17,4 @@ def authenticate_user(user: UserLogin, db: Session = Depends(get_db)) -> bool:
         return False
     if not verify_password(user.password, db_user.password):
         return False
-    return True
+    return db_user
